5f8ba1803412619976aff5da8a45b2d54bbf7ec3
SayoYami <sayozinyami@gmail.com>
2025-08-26 22:45:36 -0300

    Atualizcao

---
+++ dono/menus/comandos.js
@@ -0,0 +1,25 @@
+const menuprime = (pushname, date, NomeDoBot, hora120, pushnames, prefix) => {
+return `╭‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎
+╭━━━⫹⫺ *REM BOT - OFICIAL* ⫹⫺━━━╮
+│
+│        〘 🛒 *SYSTEM ADS* 🛒 〙  
+│
+│炎⃟͓🛍️➬ ${prefix}addads "recomendo"
+│炎⃟͓🛍️➬ ${prefix}rmads "recomendo"
+│炎⃟͓🛍️➬ ${prefix}listads "recomendo"
+│炎⃟͓🛍️➬ ${prefix}startads "recomendo"
+│炎⃟͓🛍️➬ ${prefix}setadsgp
+│炎⃟͓🛍️➬ ${prefix}rmadsgp
+│炎⃟͓🛍️➬ ${prefix}listadsgp
+│炎⃟͓🛍️➬ ${prefix}iddogp " pra pegar os id dos grupos"
+│炎⃟͓🛍️➬ ${prefix}addmsg
+│炎⃟͓🛍️➬ ${prefix}rmmsg
+│炎⃟͓🛍️➬ ${prefix}listmsg
+│炎⃟͓🛍️➬ ${prefix}infomsg " Olhe como usar cada Cmd "
+│  𝑓𝑎𝑐̧𝑎 𝑏𝑜𝑚 𝐴𝑝𝑟𝑜𝑣𝑒𝑖𝑡𝑜. ฅ(•- •マ
+│╰━━───────────────━╯  
+╰━━━━━━━━━━━━━━━━━━━━╯
+`
+}
+
+exports.menuprime = menuprime
---
+++ dono/menus/menudono.js
@@ -0,0 +1,69 @@
+const menudono = (sender, prefix) => {
+return `
+╭━━━ᥫ᭡ *REM BOT - OFICIAL* ᥫ᭡━━━╮
+│╭━━━──────────────━━╮
+│╎
+   ❯      〘 *CONFIGURAR* 〙
+│╎ 
+│炎⃟͓💧ۣۜ➛${prefix}antipv " bloqueia "
+│炎⃟͓💧ۣۜ➛${prefix}antipv2 " Envia Msg "
+│炎⃟͓💧ۣۜ➛${prefix}setprefix "Trocar Prefixo Do Bot"
+│炎⃟͓💧ۣۜ➛${prefix}multiprefixo
+│炎⃟͓💧ۣۜ➛${prefix}addprefixo
+│炎⃟͓💧ۣۜ➛${prefix}delprefixo
+│炎⃟͓💧ۣۜ➛${prefix}prefixos
+│炎⃟͓💧ۣۜ➛${prefix}console
+│炎⃟͓💧ۣۜ➛${prefix}configurar-bot
+
+   ❯   〘 *MENSAGENS E ANÚNCIOS* 〙
+
+│炎⃟͓💧ۣۜ➛${prefix}addmsg
+│炎⃟͓💧ۣۜ➛${prefix}rmmsg
+│炎⃟͓💧ۣۜ➛${prefix}listmsg
+│炎⃟͓💧ۣۜ➛${prefix}addrandom
+│炎⃟͓💧ۣۜ➛${prefix}envrandom
+│炎⃟͓💧ۣۜ➛${prefix}delrandom
+│炎⃟͓💧ۣۜ➛${prefix}rmrandom
+│炎⃟͓💧ۣۜ➛${prefix}envglobal
+│炎⃟͓💧ۣۜ➛${prefix}rmglobal
+│炎⃟͓💧ۣۜ➛${prefix}tm (transmissão global)
+│炎⃟͓💧ۣۜ➛${prefix}rgtm
+│炎⃟͓💧ۣۜ➛${prefix}deltm
+│炎⃟͓💧ۣۜ➛${prefix}envmsg
+│炎⃟͓💧ۣۜ➛${prefix}msgpv
+
+
+   ❯   〘 *GRUPOS E ADMINISTRAÇÃO* 〙
+   
+│炎⃟͓💧ۣۜ➛${prefix}join (entrar no grupo)
+│炎⃟͓💧ۣۜ➛${prefix}sair (sair do grupo)
+│炎⃟͓💧ۣۜ➛${prefix}sairdogp
+│炎⃟͓💧ۣۜ➛${prefix}idgp
+│炎⃟͓💧ۣۜ➛${prefix}listagp
+│炎⃟͓💧ۣۜ➛${prefix}linkdogp
+│炎⃟͓💧ۣۜ➛${prefix}iddogp
+│炎⃟͓💧ۣۜ➛${prefix}visualizarmsg
+
+│
+   ❯   〘 *MÍDIA E PERSONALIZAÇÃO* 〙
+
+│炎⃟͓💧ۣۜ➛${prefix}fotomenu
+│炎⃟͓💧ۣۜ➛${prefix}logomenu
+│炎⃟͓💧ۣۜ➛${prefix}fundomenu
+│炎⃟͓💧ۣۜ➛${prefix}fotobot
+│炎⃟͓💧ۣۜ➛${prefix}nick-dono
+│炎⃟͓💧ۣۜ➛${prefix}nome-bot
+
+   ❯   〘 *ANÚNCIOS E PUBLICIDADE* 〙
+
+│炎⃟͓💧ۣۜ➛${prefix}addads
+│炎⃟͓💧ۣۜ➛${prefix}setads
+│炎⃟͓💧ۣۜ➛${prefix}addadsimg
+│炎⃟͓💧ۣۜ➛${prefix}rmads
+│炎⃟͓💧ۣۜ➛${prefix}listads
+│╎
+│╰━━───────────────━╯
+╰━━━━━━━━━━━━━━━━━━━━╯`
+}
+
+exports.menudono = menudono
